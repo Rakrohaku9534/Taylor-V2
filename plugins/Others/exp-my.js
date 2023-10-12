633afd26eef8767bf903281c219fa141b92fe25c
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2023-10-12 08:32:24 +0800

    Add files via upload

--- plugins/Others/exp-my.js
+++ plugins/Others/exp-my.js
@@ -1,7 +1,7 @@
-import daily from './rpg-daily.js'
-import weekly from './rpg-weekly.js'
-import monthly from './rpg-monthly.js'
-import adventure from './rpg-adventure.js'
+import daily from './RPG/rpg-daily.js'
+import weekly from './RPG/rpg-weekly.js'
+import monthly from './RPG/rpg-monthly.js'
+import adventure from './RPG/rpg-adventure.js'
 
 const inventory = {
   others: {
