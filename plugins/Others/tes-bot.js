75492614128a1306141eb3697ccf3d4c5c1615ce
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2023-10-16 03:29:05 +0800

    Add files via upload

--- plugins/Others/tes-bot.js
+++ plugins/Others/tes-bot.js
@@ -1,9 +1,21 @@
-import fs from 'fs'
 import fetch from 'node-fetch'
-let handler  = async (m, { conn, usedPrefix: _p }) => {
-let info = `*Bot aktif kak*\n@${m.sender.split('@')[0]}`
-await conn.reply(m.chat, info, m, { contextInfo: { mentionedJid: [m.sender],forwardingScore: 256,
-      isForwarded: true, externalAdReply: { title: author, body: bottime, sourceUrl: sgc, thumbnail: fs.readFileSync('./thumbnail.jpg') }}})
+let handler = async (m, {
+    conn
+}) => {
+    const response = await fetch("https://techy-api.vercel.app/api/json");
+    const data = await response.json();
+    await conn.sendReact(m.chat, "⏳", m.key);
+    await conn.sendMessage(m.chat, {
+        text: data.message,
+        contextInfo: {
+            externalAdReply: {
+                    title: "The bot is active now.",
+                    thumbnail: await (await conn.getFile("https://cdn-icons-png.flaticon.com/128/12225/12225958.png")).data
+                },
+            mentionedJid: [m.sender],
+        },
+    }, { quoted: m });
+    await conn.sendReact(m.chat, "✅", m.key);
 }
 handler.customPrefix = /^(tes|tess|test)$/i
 handler.command = new RegExp
