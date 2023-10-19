7709d575ade62ba627cf288fb9695e1367ce16bf
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2023-10-20 05:06:23 +0800

    Add files via upload

--- plugins/Information/info-privatelist.js
+++ plugins/Information/info-privatelist.js
@@ -5,35 +5,44 @@ let handler = async (m, {
     command,
     args
 }) => {
-    let pc = (Object.entries(await conn.chats).map(([nama, isi]) => {
-        return {
-            nama,
-            ...isi
-        }
-    })).filter(v => !v.nama.endsWith('g.us'))
-    let list = pc.map((chat, index) => `*${index + 1}.* wa.me/${chat.id.split('@')[0]}`).join('\n')
+    let pc = Object.entries(conn.chats)
+    .map(([nama, isi]) => ({
+        nama,
+        ...isi
+    }))
+    .filter(v => !v.nama.endsWith('g.us') && v.nama.endsWith('s.whatsapp.net'));
 
-    if (!args[0]) {
-        m.reply(`📺 Private List:\n\n${list}`)
-        return
-    }
-
-    let i = parseInt(args[0]) - 1
+if (!args[0]) {
+    let list = pc.map((chat, index) => {
+        const messagesCount = chat.messages ? Object.keys(chat.messages).length : 0;
+        return `${index + 1}. 👤 *Name:* ${chat.name || 'xxxx'}\n📞 *Number:* ${chat.id.split('@')[0] || 'xxxx'}\n🔵 *Presences:* ${chat.presences || 'xxx'}\n✉️ *Messages:* ${messagesCount}`;
+    }).join('\n\n');
+    m.reply(`📺 Private List:\n\n${list}`);
+} else {
+    let i = parseInt(args[0]) - 1;
     if (!pc[i]) {
-        return m.reply('Invalid index!')
+        return m.reply('Invalid index!');
     }
 
-    let pp = await conn.profilePictureUrl(pc[i].id, 'image')
-    let str = `*Information about ${await conn.getName(pc[i].id)}*\n\n`
-    str += `*Name:* ${pc[i].name || 'Tidak diketahui'}\n`
-    str += `*ID:* @${pc[i].id.replace('@s.whatsapp.net', '')}\n`
-    str += `*Presences:* ${pc[i].presences || 'Tidak diketahui'}\n`
+    let pp = await conn.profilePictureUrl(pc[i].id, 'image').catch(_ => false);
+    let name = pc[i].name || 'Tidak diketahui';
+    let id = pc[i].id.replace('@s.whatsapp.net', '');
+    let presences = pc[i].presences || 'Tidak diketahui';
 
-    await conn.sendFile(m.chat, pp, 'profile.jpg', str, m, null, {
-        mentions: [pc[i].id]
-    })
+    let str = `*Information about ${await conn.getName(pc[i].id)}*\n\n`;
+    str += `👤 *Name:* ${name}\n📞 *ID:* @${id}\n🔵 *Presences:* ${presences}`;
+
+    if (pp) {
+        await conn.sendFile(m.chat, pp, 'profile.jpg', str, m, null, {
+            caption: str,
+            mentions: [pc[i].id]
+        });
+    } else {
+        m.reply(str);
+    }
 }
 
+}
 handler.help = ['listpc']
 handler.tags = ['owner']
 handler.command = ['listpc']
