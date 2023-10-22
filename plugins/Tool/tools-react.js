dfdea87ae63e897a532896dd605c1ae24b8d3b71
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2023-10-22 16:52:57 +0800

    Add files via upload

--- plugins/Tool/tools-react.js
+++ plugins/Tool/tools-react.js
@@ -1,18 +1,30 @@
-let handler = async (m, { conn, usedPrefix: _p, args, text, usedPrefix}) => {
-	
-	if (!m.quoted) throw 'Balas Chatnya !'
-	if (text.length > 2) throw 'Cuma Untuk 1 Emoji!'
-	if (!text) throw `📍 Contoh Penggunaan :\n${usedPrefix}react 🗿`
-conn.relayMessage(m.chat, { reactionMessage: {
-key: {
- id: m.quoted.id,
- remoteJid: m.chat,
- fromMe: true
-},
- text: `${text}`}}, { messageId: m.id })
- }
- handler.help = ['react <emoji>']
-handler.tags = ['Baileys']
-handler.command = /^(reac(tions|(t(ion)?)?)|addreact)$/i
+let handler = async (m, { conn, usedPrefix: _p, args, text, usedPrefix }) => {
+  try {
+    if (!m.quoted) throw 'Balas Chatnya !';
+    if (text.length > 2) throw 'Cuma Untuk 1 Emoji!';
+    if (!text) throw `📍 Contoh Penggunaan :\n${usedPrefix}react 🗿`;
 
-export default handler
+    await conn.relayMessage(m.chat, {
+      reactionMessage: {
+        key: {
+          id: m.quoted.id,
+          remoteJid: m.chat,
+          fromMe: true
+        },
+        text: text
+      }
+    }, { messageId: m.id });
+  } catch (error) {
+    try {
+    await conn.sendReact(m.chat, text, m.quoted.vM.key);
+  } catch (error) {
+    await conn.reply(m.chat, eror, m);
+  }
+  }
+};
+
+handler.help = ['react <emoji>'];
+handler.tags = ['Baileys'];
+handler.command = /^(reac(tions|(t(ion)?)?)|addreact)$/i;
+
+export default handler;
