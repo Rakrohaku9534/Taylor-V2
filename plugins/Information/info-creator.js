b634b9f55395e11f6d4d8756a71fa8e3a23a89dd
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2023-10-22 16:50:25 +0800

    Add files via upload

--- plugins/Information/info-creator.js
+++ plugins/Information/info-creator.js
@@ -1,4 +1,5 @@
 import fetch from "node-fetch"
+import PhoneNumber from 'awesome-phonenumber'
 let handler = async (m, {
     conn,
     usedPrefix,
@@ -18,6 +19,16 @@ let handler = async (m, {
                 contacts: [{
                     vcard
                 }]
+            },
+            contextInfo: {
+                externalAdReply: {
+                    title: wm,
+                    mediaType: 1,
+                    previewType: 0,
+                    renderLargerThumbnail: true,
+                    thumbnailUrl: logo,
+                    sourceUrl: ''
+                }
             }
         }, {
             quoted: fakes
@@ -28,41 +39,100 @@ let handler = async (m, {
     }
     if (command == "pengembang") {
         let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;${author};;;\nFN:${author}\nORG:${author}\nTITLE:\nitem1.TEL;waid=6282195322106:+62 821-9532-2106\nitem1.X-ABLabel:${author}\nX-WA-BIZ-DESCRIPTION:${htjava} Nih pengembang ku kack yg mengaktifkan aq.\nX-WA-BIZ-NAME:${author}\nEND:VCARD`
-        await conn.sendMessage(m.chat, {
+        let tag_own = await conn.sendMessage(m.chat, {
             contacts: {
                 displayName: wm,
                 contacts: [{
                     vcard
                 }]
+            },
+            contextInfo: {
+                externalAdReply: {
+                    title: wm,
+                    mediaType: 1,
+                    previewType: 0,
+                    renderLargerThumbnail: true,
+                    thumbnailUrl: logo,
+                    sourceUrl: ''
+                }
             }
         }, {
             quoted: fakes
         })
+        await conn.reply(m.chat, `Halo kak @${m.sender.split("@")[0]} itu nomor team developerku, jangan di apa-apain ya kak😖`, tag_own, {
+            mentions: [m.sender]
+        })
     }
     if (command == "owner") {
         try {
-    const ownerPromises = global.owner.map(async (item, index) => [
-        item[0],
-        (await conn.getName(item[0] + "@s.whatsapp.net")) || "Tidak diketahui",
-        "👑 Owner",
-        ((await conn.fetchStatus(item[0] + "@s.whatsapp.net")).status) || "Tidak diketahui",
-        "wudysoft@gmail.com",
-        "🇮🇩 Indonesia",
-        "🚀 https://aygemuy.github.io/",
-        "👤 Gada pawang nih senggol dong 😔"
-    ]);
+            const ownerPromises = global.owner.map(async (item, index) => [
+                item[0],
+                (await conn.getName(item[0] + "@s.whatsapp.net")) || "Tidak diketahui",
+                "👑 Owner",
+                ((await conn.fetchStatus(item[0] + "@s.whatsapp.net")).status) || "Tidak diketahui",
+                "wudysoft@gmail.com",
+                "🇮🇩 Indonesia",
+                "🚀 https://aygemuy.github.io/",
+                "👤 Gada pawang nih senggol dong 😔"
+            ]);
 
-    const resultArray = await Promise.all(ownerPromises);
-    let sentMsg = await conn.sendContactArray(m.chat, resultArray, m);
-    await conn.reply(m.chat, `Halo kak @${m.sender.split("@")[0]} itu nomor team developerku, jangan di apa-apain ya kak😖`, sentMsg, {
-        mentions: [m.sender]
-    });
-} catch {
-    let sentMsg = await conn.sendContact(m.chat, nomorown, await conn.getName(nomorown + "@s.whatsapp.net"), m);
-    await conn.reply(m.chat, `Halo kak @${m.sender.split("@")[0]} itu nomor team developerku, jangan di apa-apain ya kak😖`, sentMsg, {
-        mentions: [m.sender]
-    });
-}
+            const data = await Promise.all(ownerPromises);
+
+            let contacts = []
+            for (let [number, name, isi, isi1, isi2, isi3, isi4, isi5] of data) {
+                number = number.replace(/[^0-9]/g, '')
+                let njid = number + '@s.whatsapp.net'
+                let biz = await conn.getBusinessProfile(njid).catch(_ => null) || {}
+                // N:;${name.replace(/\n/g, '\\n').split(' ').reverse().join(';')};;;
+                let vcard = `
+BEGIN:VCARD
+VERSION:3.0
+N:Sy;Bot;;;
+FN:${name.replace(/\n/g, '\\n')}
+item.ORG:${isi}
+item1.TEL;waid=${number}:${PhoneNumber('+' + number).getNumber('international')}
+item1.X-ABLabel:${isi1}
+item2.EMAIL;type=INTERNET:${isi2}
+item2.X-ABLabel:📧 Email
+item3.ADR:;;${isi3};;;;
+item3.X-ABADR:ac
+item3.X-ABLabel:📍 Region
+item4.URL:${isi4}
+item4.X-ABLabel:Website
+item5.X-ABLabel:${isi5}
+END:VCARD`.trim()
+                contacts.push({
+                    vcard,
+                    displayName: name
+                })
+            }
+            let tag_own = await conn.sendMessage(m.chat, {
+                contacts: {
+                    displayName: (contacts.length > 1 ? `2013 kontak` : contacts[0].displayName) || null,
+                    contacts,
+                },
+                contextInfo: {
+                    externalAdReply: {
+                        title: wm,
+                        mediaType: 1,
+                        previewType: 0,
+                        renderLargerThumbnail: true,
+                        thumbnailUrl: logo,
+                        sourceUrl: ''
+                    }
+                }
+            }, {
+                quoted: fakes
+            })
+            await conn.reply(m.chat, `Halo kak @${m.sender.split("@")[0]} itu nomor team developerku, jangan di apa-apain ya kak😖`, tag_own, {
+                mentions: [m.sender]
+            });
+        } catch {
+            let tag_own = await conn.sendContact(m.chat, nomorown, await conn.getName(nomorown + "@s.whatsapp.net"), m);
+            await conn.reply(m.chat, `Halo kak @${m.sender.split("@")[0]} itu nomor team developerku, jangan di apa-apain ya kak😖`, tag_own, {
+                mentions: [m.sender]
+            });
+        }
 
     }
 }
