54503360e7fe582d720cab7907dd6184e01ba5a0
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2023-10-26 07:24:25 +0800

    Add files via upload

--- plugins/Sticker/sticker-ttp-attp.js
+++ plugins/Sticker/sticker-ttp-attp.js
@@ -1,9 +1,5 @@
-import { createSticker } from 'wa-sticker-formatter';
 import fetch from 'node-fetch';
-import {
-    ttp,
-    attp
-} from '../../lib/text2picture.js';
+const { ttp, attp } = await(await import('../../lib/text2picture.js'));
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     try {
@@ -12,42 +8,85 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
         if (!text) throw `Example: ${usedPrefix + command} Lagi Ruwet`;
 
         const apiEndpoint = 'https://api.lolhuman.xyz/api';
-        const attpRes = await fetch(`${apiEndpoint}/${command}?apikey=${global.lolkey}&text=${encodeURIComponent(text.substring(0, 151))}`);
-        const ttp7Res = await (await fetch(global.API('https://salism3api.pythonanywhere.com', '/text2img/', { text: text }))).json();
-        const ttp8Res = await (await fetch(global.API('https://salism3api.pythonanywhere.com', '/text2img/', { text: text, outlineColor: '255,0,0,255', textColor: '0,0,0,255' }))).json();
+        const apiKey = global.lolkey;
 
-        let stiker;
-        if (command === 'attp' || command === 'attp3' || command === 'hartacustom') {
-            stiker = await createSticker(await attpRes.buffer(), { pack: packname, author: author });
-        } else if (command === 'attp2') {
-            const stick = (await (await fetch(global.API('https://salism3api.pythonanywhere.com', '/text2img/', { text: text }))).json()).image;
-            stiker = await createSticker(stick, { pack: packname, author: author });
-        } else if (command === 'attp5') {
-            const stick = await attp(text);
-            stiker = await createSticker(stick[0].url, { pack: packname, author: author });
-        } else if (command === 'ttp7') {
-            const stick = ttp7Res.image;
-            stiker = await createSticker(stick, { pack: packname, author: author });
-        } else if (command === 'ttp8') {
-            const stick = ttp8Res.image;
-            stiker = await createSticker(stick, { pack: packname, author: author });
-        } else if (command === 'ttp9') {
-            const stick = await ttp(text);
-            stiker = await createSticker(stick[0].url, { pack: packname, author: author });
-        } else {
-            stiker = await createSticker(await attpRes.buffer(), { pack: packname, author: author });
+        const commandMap = {
+            ttp: async () => {
+                const apiUrl = `${apiEndpoint}/ttp?apikey=${apiKey}&text=${encodeURIComponent(text.substring(0, 151))}`;
+                const attpRes = await fetch(apiUrl);
+                return attpRes.arrayBuffer();
+            },
+            ttp2: async () => {
+                const apiUrl = `${apiEndpoint}/ttp2?apikey=${apiKey}&text=${encodeURIComponent(text.substring(0, 151))}`;
+                const attpRes = await fetch(apiUrl);
+                return attpRes.arrayBuffer();
+            },
+            ttp3: async () => {
+                const apiUrl = `${apiEndpoint}/ttp3?apikey=${apiKey}&text=${encodeURIComponent(text.substring(0, 151))}`;
+                const attpRes = await fetch(apiUrl);
+                return attpRes.arrayBuffer();
+            },
+            ttp4: async () => {
+                const apiUrl = `${apiEndpoint}/ttp4?apikey=${apiKey}&text=${encodeURIComponent(text.substring(0, 151))}`;
+                const attpRes = await fetch(apiUrl);
+                return attpRes.arrayBuffer();
+            },
+            ttp5: async () => {
+                const apiUrl = `${apiEndpoint}/ttp5?apikey=${apiKey}&text=${encodeURIComponent(text.substring(0, 151))}`;
+                const attpRes = await fetch(apiUrl);
+                return attpRes.arrayBuffer();
+            },
+            ttp6: async () => {
+                const apiUrl = `${apiEndpoint}/ttp6?apikey=${apiKey}&text=${encodeURIComponent(text.substring(0, 151))}`;
+                const attpRes = await fetch(apiUrl);
+                return attpRes.arrayBuffer();
+            },
+            ttp7: async () => {
+                const response = await ttp(text);
+                if (Array.isArray(response) && response.length > 0 && response[0].url) {
+                    return await fetch(response[0].url).then(res => res.arrayBuffer());
+                } else {
+                    throw 'Invalid response from ttp7';
+                }
+            },
+            attp: async () => {
+                const apiUrl = `${apiEndpoint}/attp?apikey=${apiKey}&text=${encodeURIComponent(text)}`;
+                const attpRes = await fetch(apiUrl);
+                return attpRes.arrayBuffer();
+            },
+            attp2: async () => {
+                const apiUrl = `${apiEndpoint}/attp2?apikey=${apiKey}&text=${encodeURIComponent(text)}`;
+                const attpRes = await fetch(apiUrl);
+                return attpRes.arrayBuffer();
+            },
+            attp3: async () => {
+                const response = await attp(text);
+                if (Array.isArray(response) && response.length > 0 && response[0].url) {
+                    return await fetch(response[0].url).then(res => res.arrayBuffer());
+                } else {
+                    throw 'Invalid response from attp3';
+                }
+            },
+        };
+
+        const commandFunction = commandMap[command];
+
+        if (!commandFunction) {
+            throw 'Invalid command';
         }
 
-        await conn.sendFile(m.chat, stiker, 'atet.webp', '', m);
+        const data = await commandFunction();
+        
+        await conn.sendFile(m.chat, data, 'atet.webp', '', m);
     } catch (error) {
         console.error(error);
         m.reply(eror);
     }
 };
 
-handler.help = ['ttp', 'ttp2 -> ttp9', 'attp', 'attp2 -> attp5', 'hartacustom'];
-handler.tags = ['creator'];
-handler.command = /^((ttp(2|3|4|5|6|7|8|9)?)|(attp(2|3|4|5)?))|(hartacustom)$/i;
+handler.help = ['ttp', 'ttp2 -> ttp7', 'attp', 'attp2 -> attp3', 'hartacustom'];
+handler.tags = ['sticker'];
+handler.command = /^(ttp[2-7]?|attp[2-3]?|hartacustom)$/i;
 handler.limit = true;
 
 export default handler;
