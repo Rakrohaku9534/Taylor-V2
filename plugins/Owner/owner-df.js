269899d69452a5378a4f491e82fdfff612a487ee
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2023-10-29 15:24:17 +0800

    Add files via upload

--- plugins/Owner/owner-df.js
+++ plugins/Owner/owner-df.js
@@ -1,45 +1,63 @@
 import { exec } from 'child_process';
 import { promisify } from 'util';
 
-let handler = async (m, { conn, isROwner, usedPrefix, command, text }) => {
-  // Menunggu pesan "global.wait" sebelum melanjutkan
-  await m.reply(global.wait);
+// Daftar plugin yang diberikan
+const pluginsList = Object.keys(global.plugins);
 
-  // Memeriksa apakah pengguna adalah pemilik bot
-  if (!isROwner) return;
+let handler = async (m, { conn, command, args }) => {
+        if (args.length === 0) {
+            // Menampilkan list kategori
+            const pluginCategories = [...new Set(pluginsList.map(plugin => plugin.split('/')[2]))];
+            const pluginList = pluginCategories.map((category, index) => `${index + 1}. *${category}*`).join('\n');
+            await m.reply(`*List Kategori:*\n${pluginList}`);
+        } else if (args.length === 1) {
+            // Menampilkan list file dalam kategori tertentu
+            const categoryIndex = parseInt(args[0]) - 1;
+            const pluginCategories = [...new Set(pluginsList.map(plugin => plugin.split('/')[2]))];
 
-  // Mendapatkan daftar plugin
-  const array = Object.keys(plugins);
-  const execAsync = promisify(exec);
-  const input = text;
+            if (!isNaN(categoryIndex) && categoryIndex >= 0 && categoryIndex < pluginCategories.length) {
+                const selectedCategory = pluginCategories[categoryIndex];
+                const pluginNames = pluginsList.filter(plugin => plugin.split('/')[2] === selectedCategory);
+                const pluginList = pluginNames.map((plugin, index) => `${index + 1}. *${plugin.split('/').pop().replace('.js', '')}*`).join('\n');
+                await m.reply(`*List File dalam Kategori "${selectedCategory}":*\n${pluginList}`);
+            } else {
+                await m.reply('*Input salah* atau angka di luar rentang yang sesuai.');
+            }
+        } else if (args.length === 2 && !isNaN(args[0]) && !isNaN(args[1])) {
+            // Menjalankan operasi sesuai dengan format "df 1 2"
+            const categoryIndex = parseInt(args[0]);
+            const pluginIndex = parseInt(args[1]);
+            const pluginCategories = [...new Set(pluginsList.map(plugin => plugin.split('/')[2]))];
 
-  // Validasi input untuk menghindari karakter khusus atau instruksi berbahaya
-  if (!/^[a-zA-Z0-9-_.]+$/.test(input)) {
-    await m.reply("Input tidak valid. Harap gunakan karakter yang aman.");
-    return;
-  }
+            if (categoryIndex >= 1 && categoryIndex <= pluginCategories.length) {
+                const selectedCategory = pluginCategories[categoryIndex - 1];
+                const pluginNames = pluginsList.filter(plugin => plugin.split('/')[2] === selectedCategory);
 
-  const index = array.findIndex(item => item.includes(input));
+                if (pluginIndex >= 1 && pluginIndex <= pluginNames.length) {
+                    const selectedPlugin = pluginNames[pluginIndex - 1];
+                    const commandToExecute = `rm -rf '${selectedPlugin}'`;
 
-  if (index !== -1) {
-    const fileToCat = array[index];
-    try {
-      const { stdout, stderr } = await execAsync(`rm -rf ${fileToCat}`);
-      const output = stdout || stderr;
-      await m.reply(output);
-    } catch (error) {
-      const errorMessage = `Terjadi kesalahan: ${error.message}`;
-      await m.reply(errorMessage);
+                    const execPromise = promisify(exec);
+                    try {
+                        const { stdout, stderr } = await execPromise(commandToExecute);
+                        await m.reply(`*Sukses deleted*\n${selectedPlugin}\n${stdout}`);
+                    } catch (error) {
+                        await m.reply(`*Error:* ${error.message}`);
+                    }
+                } else {
+                    await m.reply('*Indeks plugin salah* atau di luar rentang yang sesuai.');
+                }
+            } else {
+                await m.reply('*Indeks kategori salah* atau di luar rentang yang sesuai.');
+            }
+        } else {
+            await m.reply('*Format perintah tidak valid.* Gunakan "df" untuk menampilkan list kategori, "df 1" untuk menampilkan list file dalam kategori, atau "df 1 2" untuk menjalankan operasi tertentu.');
+        }
     }
-  } else {
-    const notFoundMessage = `Input ${input} tidak ditemukan dalam array. Berikut adalah daftar dengan nomor urutan:\n${array.map(item => item.split("/").pop().replace(".js", "")).map((item, i) => `${i + 1}. ${item}`).join('\n')}\nContoh penggunaan: Untuk mencari bagian, ketik *.df anti-audio*`;
-    await m.reply(notFoundMessage);
-  }
-}
 
-handler.help = ['dfplugin'].map(v => v + ' <text>')
+handler.help = ['df'].map(v => v + ' *index*')
 handler.tags = ['owner']
-handler.command = /^(dfplugin|df)$/i
+handler.command = /^(df)$/i
 handler.rowner = true
 
 export default handler
