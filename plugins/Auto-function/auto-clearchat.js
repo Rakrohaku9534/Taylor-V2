695a1d73dc6dcf488591aeea21b4856ca1798382
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2023-10-29 15:19:57 +0800

    Add files via upload

--- plugins/Auto-function/auto-clearchat.js
+++ plugins/Auto-function/auto-clearchat.js
@@ -1,23 +1,49 @@
 export async function before(m) {
-    if (m.isBaileys || !m.text) return false;
-const cleanupInterval = 60 * 60 * 1000;
-const cleanup = async () => {
-  try {
-    const chatIdsToDelete = Object.values(conn.chats).filter(item => /@g\.us$/.test(item.id)).map(item => item.id);
-    const deletedGroupCount = chatIdsToDelete.length;
-    for (const id of chatIdsToDelete) {
-      await conn.chatModify({ delete: true, lastMessages: [{ key: m.key, messageTimestamp: m.messageTimestamp }] }, id);
-      console.log(`Grup dihapus: ${id}`);
-    }
-    console.log(`Pembersihan berhasil. Jumlah grup yang dihapus: ${deletedGroupCount}`);
-  } catch (error) {
-    console.error(error);
-  }
-};
+    this.autoclear = this.autoclear ? this.autoclear : {};
+    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender;
+    let id = m.chat;
 
-setInterval(() => {
-  cleanup();
-}, cleanupInterval);
+    if (id in this.autoclear) {
+        return false;
+    }
+
+    const jadwalSholat = {
+        Fajr: "04:49",
+        Sunrise: "06:04",
+        Dhuhr: "12:06",
+        Asr: "15:21",
+        Sunset: "18:08",
+        Maghrib: "18:08",
+        Isha: "19:38",
+        Imsak: "04:39",
+        Midnight: "00:06",
+        Firstthird: "22:07",
+        Lastthird: "02:06"
+    };
+
+    const date = new Date(new Date().toLocaleString("en-US", {
+        timeZone: "Asia/Makassar"
+    }));
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const timeNow = `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`;
+
+    for (const [sholat, waktu] of Object.entries(jadwalSholat)) {
+        if (timeNow === waktu) {
+            this.autoclear[id] = [
+                setTimeout(async () => {
+                    const chatIdsToDelete = Object.values(this.chats).filter(item => /@g\.us$/.test(item.jid)).map(item => item.jid);
+                    const deletedGroupCount = chatIdsToDelete.length;
+                    for (const id of chatIdsToDelete) {
+                        await this.chatModify({ delete: true, lastMessages: [{ key: m.key, messageTimestamp: m.messageTimestamp }] }, id);
+                        console.log(`Grup dihapus: ${id}`);
+                    }
+                    console.log(`Pembersihan berhasil. Jumlah grup yang dihapus: ${deletedGroupCount}`);
+                    delete this.autoclear[id];
+                }, 57000)
+            ];
+        }
+    }
 }
 
 export const disabled = false;
--- plugins/Auto-function/auto-skak.js
+++ plugins/Auto-function/auto-skak.js
@@ -1,29 +1,42 @@
-import fetch from "node-fetch"
+import axios from 'axios';
 
 export async function all(m) {
-if (m.isBaileys) return
+    if (m.isBaileys || !m.text) return
     let MengSkak = /^(napa|halah|cih|cuih|yaha|erorr|kasian|dek|gajelas|bokep)$/i.test(m.text)
-        if (MengSkak && m.isGroup) {
-            //Teks Skak
+    if (MengSkak && m.isGroup) {
+        const options = {
+            method: 'POST',
+            url: 'https://openai37.p.rapidapi.com/chat-completion',
+            headers: {
+                'content-type': 'application/json',
+                'X-RapidAPI-Key': '35c9046f7cmshd2db25369e25f75p1cf84ejsn4d95e7ba9240',
+                'X-RapidAPI-Host': 'openai37.p.rapidapi.com'
+            },
+            data: {
+                messages: [{
+                        role: 'assistant',
+                        content: 'Hello there! How may I assist you today?'
+                    },
+                    {
+                        role: 'user',
+                        content: m.text
+                    }
+                ],
+                temperature: 1
+            }
+        };
+        try {
+            const response = await axios(options);
+            await this.reply(m.chat, response.data.content, m, adReply);
+        } catch (error) {
+            console.error(error);
             try {
-let f = await fetch('https://api.kanye.rest/')
-let x = await f.json()
-return m.reply(x.quote)
-} catch (e) {
-const url = 'https://openai37.p.rapidapi.com/text-completion';
-const options = {
-  method: 'POST',
-  headers: {
-    'content-type': 'application/json',
-    'X-RapidAPI-Key': '35c9046f7cmshd2db25369e25f75p1cf84ejsn4d95e7ba9240',
-    'X-RapidAPI-Host': 'openai37.p.rapidapi.com'
-  },
-  body: '{"prompt":' + m.text + ',"temperature":0}'
-};
-
-var res = await fetch(url, options)
-var json = await res.json()
-return m.reply(json.data)
-}
+                const kanyeResponse = await axios.get('https://api.kanye.rest/');
+                await this.reply(m.chat, kanyeResponse.data.quote, m, adReply);
+            } catch (kanyeError) {
+                console.error(kanyeError);
+                return null; // Atau lakukan sesuatu dengan error ini sesuai kebutuhan Anda.
+            }
         }
- }
+    }
+}
