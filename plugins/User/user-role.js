10293f25419aaab34474e790e2b6353336f39503
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2023-10-30 21:53:34 +0800

    Add files via upload

--- plugins/User/user-role.js
+++ plugins/User/user-role.js
@@ -1,56 +1,71 @@
-const roles = {
-    /*
-    'Role Name': <Minimal Level To Obtain this Role>
-    */
-    'Bronze V': 0,
-    'Bronze IV': 5,
-    'Bronze III': 10,
-    'Bronze II': 15,
-    'Bronze I': 20,
-    'Elite V': 25,
-    'Elite IV': 30,
-    'Elite III': 35,
-    'Elite II': 40,
-    'Elite I': 45,
-    'Master V': 50,
-    'Master IV': 55,
-    'Master III': 60,
-    'Master II': 65,
-    'Master I': 70,
-    'Grand Master V': 75,
-    'Grand Master IV': 80,
-    'Grand Master III': 85,
-    'Grand Master II': 90,
-    'Grand Master I': 95,
-    'Epic V': 100,
-    'Epic IV': 105,
-    'Epic III': 110,
-    'Epic II': 115,
-    'Epic I': 120,
-    'Legend V': 125,
-    'Legend IV': 130,
-    'Legend III': 135,
-    'Legend II': 140,
-    'Legend I': 145,
-    'Mythic V': 150,
-    'Mythic IV': 155,
-    'Mythic III': 160,
-    'Mythic II': 165,
-    'Mythic I': 170,
-    'Mythic Glory': 175,
-    'EMERALD V': 180,
-    'EMERALD IV': 185,
-    'EMERALD III': 190,
-    'EMERALD II': 195,
-    'EMERALD I': 200,
-    'THE EMERALD': 205
-}
-
-export function before(m) {
+export async function before(m) {
+	try {
         let user = global.db.data.users[m.sender]
-        let level = user.level
-        let role = (Object.entries(roles).sort((a, b) => b[1] - a[1]).find(([, minLevel]) => level >= minLevel) || Object.entries(roles)[0])[0]
+        let role = (user.level <= 2) ? 'Newbie ㋡'
+          : ((user.level >= 2) && (user.level <= 4)) ? 'Beginner Grade 1 ⚊¹'
+          : ((user.level >= 4) && (user.level <= 6)) ? 'Beginner Grade 2 ⚊²'
+          : ((user.level >= 6) && (user.level <= 8)) ? 'Beginner Grade 3 ⚊³'
+          : ((user.level >= 8) && (user.level <= 10)) ? 'Beginner Grade 4 ⚊⁴'
+          : ((user.level >= 10) && (user.level <= 20)) ? 'Private Grade 1 ⚌¹'
+          : ((user.level >= 20) && (user.level <= 30)) ? 'Private Grade 2 ⚌²'
+          : ((user.level >= 30) && (user.level <= 40)) ? 'Private Grade 3 ⚌³'
+          : ((user.level >= 40) && (user.level <= 50)) ? 'Private Grade 4 ⚌⁴'
+          : ((user.level >= 50) && (user.level <= 60)) ? 'Private Grade 5 ⚌⁵'
+          : ((user.level >= 60) && (user.level <= 70)) ? 'Corporal Grade 1 ☰¹' 
+          : ((user.level >= 70) && (user.level <= 80)) ? 'Corporal Grade 2 ☰²' 
+          : ((user.level >= 80) && (user.level <= 90)) ? 'Corporal Grade 3 ☰³' 
+          : ((user.level >= 90) && (user.level <= 100)) ? 'Corporal Grade 4 ☰⁴' 
+          : ((user.level >= 100) && (user.level <= 110)) ? 'Corporal Grade 5 ☰⁵'
+          : ((user.level >= 110) && (user.level <= 120)) ? 'Sergeant Grade 1 ≣¹'
+          : ((user.level >= 120) && (user.level <= 130)) ? 'Sergeant Grade 2 ≣²'
+          : ((user.level >= 130) && (user.level <= 140)) ? 'Sergeant Grade 3 ≣³'
+          : ((user.level >= 140) && (user.level <= 150)) ? 'Sergeant Grade 4 ≣⁴'
+          : ((user.level >= 150) && (user.level <= 160)) ? 'Sergeant Grade 5 ≣⁵' 
+          : ((user.level >= 160) && (user.level <= 170)) ? 'Staff Grade 1 ﹀¹' 
+          : ((user.level >= 170) && (user.level <= 180)) ? 'Staff Grade 2 ﹀²' 
+          : ((user.level >= 180) && (user.level <= 190)) ? 'Staff Grade 3 ﹀³' 
+          : ((user.level >= 190) && (user.level <= 200)) ? 'Staff Grade 4 ﹀⁴' 
+          : ((user.level >= 200) && (user.level <= 210)) ? 'Staff Grade 5 ﹀⁵' 
+          : ((user.level >= 210) && (user.level <= 220)) ? 'Sergeant Grade 1 ︾¹'
+          : ((user.level >= 220) && (user.level <= 230)) ? 'Sergeant Grade 2 ︾²'
+          : ((user.level >= 230) && (user.level <= 240)) ? 'Sergeant Grade 3 ︾³'
+          : ((user.level >= 240) && (user.level <= 250)) ? 'Sergeant Grade 4 ︾⁴'
+          : ((user.level >= 250) && (user.level <= 260)) ? 'Sergeant Grade 5 ︾⁵'
+          : ((user.level >= 260) && (user.level <= 270)) ? '2nd Lt. Grade 1 ♢¹'
+          : ((user.level >= 270) && (user.level <= 280)) ? '2nd Lt. Grade 2 ♢²'  
+          : ((user.level >= 280) && (user.level <= 290)) ? '2nd Lt. Grade 3 ♢³' 
+          : ((user.level >= 290) && (user.level <= 300)) ? '2nd Lt. Grade 4 ♢⁴' 
+          : ((user.level >= 300) && (user.level <= 310)) ? '2nd Lt. Grade 5 ♢⁵'
+          : ((user.level >= 310) && (user.level <= 320)) ? '1st Lt. Grade 1 ♢♢¹'
+          : ((user.level >= 320) && (user.level <= 330)) ? '1st Lt. Grade 2 ♢♢²'
+          : ((user.level >= 330) && (user.level <= 340)) ? '1st Lt. Grade 3 ♢♢³'
+          : ((user.level >= 340) && (user.level <= 350)) ? '1st Lt. Grade 4 ♢♢⁴'
+          : ((user.level >= 350) && (user.level <= 360)) ? '1st Lt. Grade 5 ♢♢⁵'
+          : ((user.level >= 360) && (user.level <= 370)) ? 'Major Grade 1 ✷¹'
+          : ((user.level >= 370) && (user.level <= 380)) ? 'Major Grade 2 ✷²'
+          : ((user.level >= 380) && (user.level <= 390)) ? 'Major Grade 3 ✷³'
+          : ((user.level >= 390) && (user.level <= 400)) ? 'Major Grade 4 ✷⁴'
+          : ((user.level >= 400) && (user.level <= 410)) ? 'Major Grade 5 ✷⁵'
+          : ((user.level >= 410) && (user.level <= 420)) ? 'Colonel Grade 1 ✷✷¹'
+          : ((user.level >= 420) && (user.level <= 430)) ? 'Colonel Grade 2 ✷✷²'
+          : ((user.level >= 430) && (user.level <= 440)) ? 'Colonel Grade 3 ✷✷³'
+          : ((user.level >= 440) && (user.level <= 450)) ? 'Colonel Grade 4 ✷✷⁴'
+          : ((user.level >= 450) && (user.level <= 460)) ? 'Colonel Grade 5 ✷✷⁵'
+          : ((user.level >= 460) && (user.level <= 470)) ? 'Brigadier Early ✰'
+          : ((user.level >= 470) && (user.level <= 480)) ? 'Brigadier Silver ✩'
+          : ((user.level >= 480) && (user.level <= 490)) ? 'Brigadier gold ✯' 
+          : ((user.level >= 490) && (user.level <= 500)) ? 'Brigadier Platinum ✬'
+          : ((user.level >= 500) && (user.level <= 600)) ? 'Brigadier Diamond ✪'
+          : ((user.level >= 600) && (user.level <= 700)) ? 'Legendary 忍'
+          : ((user.level >= 700) && (user.level <= 800)) ? 'Legendary 忍忍'
+          : ((user.level >= 800) && (user.level <= 900)) ? 'Legendary 忍忍忍'
+          : ((user.level >= 900) && (user.level <= 1000)) ? 'Legendary忍忍忍忍'
+          : 'Master 숒 × Legendary 숒'
         user.role = role
         return !0
-    
+} catch {
+        await this.reply(m.chat, 'Error occurred.', m);
+    }
 }
+
+export const disabled = false;
