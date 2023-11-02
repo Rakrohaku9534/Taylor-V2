58607ea603c0974d2d393a3c4ae2aafb0af780fd
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2023-11-03 04:01:18 +0800

    Add files via upload

--- plugins/Downloader/downloader-xstream.js
+++ plugins/Downloader/downloader-xstream.js
@@ -1,4 +1,7 @@
-import fetch from 'node-fetch';
+const {
+    generatePlayUrl,
+    regexPattern
+} = await (await import('../../lib/dodstream.js'));
 
 const handler = async (m, {
     conn,
@@ -6,36 +9,29 @@ const handler = async (m, {
     usedPrefix,
     command
 }) => {
-
-    const msg = `Input link atau reply link yang ingin di download!\n\n*Contoh:*\n${usedPrefix + command} link`;
+    const msg = `Input link atau reply link!\n\n*Contoh:*\n${usedPrefix + command} link`;
     let text
     if (args.length >= 1) {
         text = args.slice(0).join(" ")
     } else if (m.quoted && m.quoted.text) {
         text = m.quoted.text
-    } else throw msg;
-    
-    await conn.reply(m.chat, wait, m);
-    
-    const url = text.trim();
-const doodRegex = /(?:doodstream|dood|dooood|ds2play)(?:.*)\/(?:d\/|e\/)([A-z0-9]+)/;
-try {
-  const match = url.match(doodRegex);
-  if (match) {
-    const id = match[1];
-    const response = await fetch(`https://api.delivrjs.workers.dev/encrypt/${id}`);
-    const { encryptId } = await response.json();
-    await m.reply(`*Stream:*\nhttps://xstreampro.pages.dev/play.html?id=${encryptId}&host=doodstream`);
-  } else {
-    throw new Error('URL tidak cocok dengan regex');
-  }
-} catch (error) {
-  await conn.reply(m.chat, eror, m);
-}
+    } else return m.reply(msg)
 
+    const inputText = text.trim();
+
+    if (!inputText) return m.reply(msg)
+    const match = inputText.match(regexPattern);
+    if (!match) return m.reply("Input tidak sesuai dengan regex pattern.");
+
+    try {
+        const playUrl = await generatePlayUrl(inputText);
+        await m.reply("- *Stream:*\n" + playUrl);
+    } catch (error) {
+        console.error(error);
+        await m.reply('Terjadi kesalahan saat mengunduh video. Silakan coba lagi nanti.');
+    }
 };
-
-handler.help = ["xstream *[link]*"];
+handler.help = ["xstream"];
 handler.tags = ["downloader"];
 handler.command = /^(xstream)$/i;
 export default handler;
